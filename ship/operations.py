39d555274aaebd3d21ca1513ebac4603d80466b4
julian-zucker <julian.zucker@gmail.com>
2018-03-08 12:12:59 -0500

    order requirements
    slightly stricter on what's allowed in orders to be considered valid

--- ship/operations.py
+++ ship/operations.py
@@ -1,15 +1,41 @@
 """Functions related to ship management"""
 from ship.database import *
 
+
 def get_orders_from_ship(db, key, ship):
     return get_ship_orders(db, key, ship)
 
 
 def add_order_to_ship(db, key, ship, order):
+    if not is_valid_ship_order(order):
+        raise NotImplementedError("Invalid Order")
+
     orders = get_ship_orders(db, key, ship)
     orders.append(order)
-
     return set_ship_orders(db, key, ship, orders)
+
+
+def is_valid_ship_order(order):
+    if not order["order"]:
+        return False
+
+    def valid_attack():
+        return order["order"] == "attack" and "civ" in order \
+               and isinstance(order["civ"], list) and len(order["civ"]) > 0
+
+    def valid_beam():
+        return order["order"] == "beam" and "destination" in order and "tuning" in order
+
+    def valid_ftl():
+        return order["order"] == "ftl" and "destination" in order
+
+    def valid_sieze():
+        return order["order"] == "sieze"
+
+    def valid_suicide():
+        return order["order"] == "suicide"
+
+    return valid_attack() or valid_beam() or valid_ftl() or valid_sieze() or valid_suicide()
 
 
 def remove_all_orders_from_ship(db, key, ship):
--- ship/test_ship_operations.py
+++ ship/test_ship_operations.py
@@ -24,21 +24,66 @@ def test_sending_ship_orders(db):
     assert get_orders_from_ship(db, "key1", 1) == []
 
 
+def test_can_send_valid_orders(db):
+    order_attack_one = {"order": "attack", "civ": ["mars"]}
+    order_attack_multiple = {"order": "attack", "civ": ["mars", "earth"]}
+    order_beam = {"order": "beam", "destination": 4, "tuning": 13}
+    order_ftl = {"order": "ftl", "destination": 4}
+    order_sieze = {"order": "sieze"}
+    order_suicide = {"order": "suicide"}
+
+    orders = [order_attack_one,
+              order_attack_multiple,
+              order_beam,
+              order_ftl,
+              order_sieze,
+              order_suicide]
+
+    try:
+        for order in orders:
+            add_order_to_ship(db, "key1", 1, order)
+        success = True
+    except:
+        success = False
+
+    assert success
+
+
+def test_cant_send_invalid_orders(db):
+    order_attack_none = {"order": "attack", "civ": []}
+    order_not_in_list = {"order": "foo"}
+    order_beam_without_destination = {"order": "beam", "tuning": 213}
+    order_beam_without_tuning = {"order": "beam", "destination": 4}
+    order_beam_without_either = {"order": "beam"}
+
+    orders = [order_attack_none,
+              order_not_in_list,
+              order_beam_without_destination,
+              order_beam_without_tuning,
+              order_beam_without_either]
+
+    for order in orders:
+        try:
+            add_order_to_ship(db, "key1", 1, order)
+            assert order is False
+        except NotImplementedError as e:
+            assert True
+
+
 def test_get_ships(db):
     key1ships = get_ships(db, "key1")
     expected1 = [{"id": 1, "shipyard": 1, "location": 1, "flag": 1, "orders": []},
-                {"id": 2, "shipyard": 1, "location": 1, "flag": 1, "orders": []},
-                {"id": 3, "shipyard": 1, "location": 4, "flag": 1, "orders": []}]
+                 {"id": 2, "shipyard": 1, "location": 1, "flag": 1, "orders": []},
+                 {"id": 3, "shipyard": 1, "location": 4, "flag": 1, "orders": []}]
     assert key1ships == expected1
 
     key3ships = get_ships(db, "key3")
     expected3 = [{"id": 4, "shipyard": 3, "location": 4, "flag": 3, "orders": []}]
     assert key3ships == expected3
 
-
-    assert get_ships(db, "no such key") == None
+    assert get_ships(db, "no such key") is None
 
 
 def test_get_ship_info(db):
     assert get_ship_info(db, "key1", 1) == {"id": 1, "shipyard": 1, "location": 1, "flag": 1, "orders": []}
-    assert get_ship_info(db, "key1", 4) == None
+    assert get_ship_info(db, "key1", 4) is None
