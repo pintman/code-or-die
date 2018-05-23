107430fd622500203bb92248ef1ead65850819ac
Marco Bakera <marco@bakera.de>
2018-05-23 14:28:24 +0200

    removed alias, added endless loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,13 @@
 DATABASE=code_or_die
 
 /etc/init.d/postgresql start
-alias pg_ctl=pg_ctlcluster
 
 cd /code-or-die
 su -c "psql -c \"CREATE DATABASE $DATABASE \"" postgres
 mkdir -p /var/lib/postgresql/9.4/main/logs
-#sleep 10
 su -c make postgres
+
+while true; do
+	echo "running in endless loop"; 
+	sleep 60;
+done
